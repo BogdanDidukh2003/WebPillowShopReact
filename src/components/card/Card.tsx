b7cf618433444cd57c53e864fba9de0ed6732ed7
Богдан Дідух <74701445+BogdanDidukh2003@users.noreply.github.com>
2021-11-22 10:45:27 +0200

    Add catalog page

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -5,15 +5,12 @@ interface CardProps {
     pillow: Pillow;
 }
 export const Card = (props: CardProps) => {
-    const { title, description, price, width, height, imgUrl } = props.pillow;
+    const { title, description, imgUrl } = props.pillow;
     return (
         <div style={{ display: "flex", flexDirection: "column", maxWidth: "300px", textAlign: "left" }}>
             <img style={{ maxHeight: "200px" }} src={imgUrl} alt="card" />
             <h5>{title}</h5>
             <p>{description}</p>
-            <p>Price: ${price}</p>
-            <p>Width: {width} sm</p>
-            <p>Height: {height} sm</p>
             <div style={{ flexDirection: "row" }}>
                 <Button label="Edit" />
                 <Button label="Delete" />
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,9 +4,9 @@ import { HeaderItem } from "./HeaderItem";
 export const Header = () => {
     return (
         <div style={{ width: "80%", marginLeft: "10%", display: "flex", justifyContent: "space-between" }}>
-            <HeaderItem label="Home" />
-            <HeaderItem label="Catalog" />
-            <HeaderItem label="Cart" />
+            <HeaderItem label="Home" path='/home' />
+            <HeaderItem label="Catalog" path='/catalog' />
+            <HeaderItem label="Cart" path='/cart' />
         </div>
     );
 };
